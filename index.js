d9501b75c8a8827069f1bdd75eec6342125b2371
Steven <stevenawhitton@gmail.com>
2021-02-02 14:17:52 +0100

    Add files for week 1 assignments

---
+++ index.js
@@ -0,0 +1,5 @@
+const oneLinerJoke = require('one-liner-joke');
+const getRandomJoke = oneLinerJoke.getRandomJoke().body
+
+console.log('Hello World 👋');
+console.log('Here is a joke:', getRandomJoke)
