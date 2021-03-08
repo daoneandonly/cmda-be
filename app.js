138a829ba46e7c622e06af9ba0a78d2fea781a7b
Steven <stevenawhitton@gmail.com>
2021-03-08 18:58:04 +0100

    Add port for deployment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const pug = require('pug');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require('path');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
